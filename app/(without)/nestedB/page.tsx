5dd65b260ecdab639bc00eff0a956df5db25f58a
gerson aguiar <gersoncafilho@gmail.com>
2024-05-29 20:24:05 -0300

    understanding page,routes and layout

---
+++ app/(without)/nestedB/page.tsx
@@ -0,0 +1,7 @@
+const Page = () => {
+    return (
+        <div>without nested</div>
+    )
+}
+
+export default Page
---
+++ app/users/[userId]/page.tsx
@@ -0,0 +1,13 @@
+interface UserIdPageProps {
+    params: {
+        userId: string;
+    }
+}
+
+const Page = ({ params }: UserIdPageProps) => {
+    return (
+        <div>User ID: {params.userId}</div>
+    )
+}
+
+export default Page
